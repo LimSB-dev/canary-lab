fd631152f9328940a8d264d6b8e5faa5dbc2eb92
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-01-14 16:35:46 +0900

    Add PWA support and configure image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+const nextPWA = {
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+  },
+};
+
+const nextConfig = {
+  images: {
+    domains: ["*"],
+  },
+  env: {},
+};
+
+module.exports = withPWA({
+  ...nextPWA,
+});
+
+module.exports = nextConfig;

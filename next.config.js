81a3d1495913a7789b7bc7a76c8e851d46a7bbe3
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-01-16 20:22:27 +0900

    Refactor next.config.js to enable PWA and set reactStrictMode to true

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
-
-const nextPWA = {
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
-  },
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig = {
   images: {
     domains: ["*"],
   },
   env: {},
+  reactStrictMode: true,
 };
 
-module.exports = withPWA({
-  ...nextPWA,
-});
-
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);

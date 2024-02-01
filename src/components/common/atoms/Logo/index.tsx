8c5877ca2f1d63722dbd7fa6199715ad921ba518
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-02-01 23:51:00 +0900

    Add layout and page components for posts

---
+++ src/components/common/atoms/Logo/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <Link href="/" passHref>
+      <Image src="/favicon.ico" alt="logo" width={16} height={16} />
+    </Link>
+  );
+};
+
+export default Logo;

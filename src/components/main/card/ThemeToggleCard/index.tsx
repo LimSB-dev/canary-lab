b98b662f1ca4bdf6f7f7117098c96e0ad523f0b9
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-02-02 02:02:45 +0900

    Refactor theme toggle functionality and improve button styling

--- src/components/main/card/ThemeToggleCard/index.tsx
+++ src/components/main/card/ThemeToggleCard/index.tsx
@@ -1,26 +1,34 @@
 "use client";
 
+import { setTheme } from "@/store/modules/theme";
 import styles from "./styles.module.scss";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook";
 
 export const ThemeToggleCard = ({ theme }: { theme: "light" | "dark" }) => {
+  const dispatch = useAppDispatch();
+  const curTheme = useAppSelector((state) => state.theme.theme);
+
   const themeToggle = () => {
     if (theme === "light") {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "dark");
-    } else {
       document.documentElement.setAttribute("data-theme", "light");
       localStorage.setItem("theme", "light");
+      dispatch(setTheme("light"));
+    } else {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+      dispatch(setTheme("dark"));
     }
   };
 
   return (
-    <article
+    <button
       className={theme === "light" ? styles.card_light : styles.card_dark}
       onClick={themeToggle}
+      disabled={curTheme === theme}
     >
       <h2 className={theme === "light" ? styles.text_light : styles.text_dark}>
         {theme.toUpperCase()}
       </h2>
-    </article>
+    </button>
   );
 };

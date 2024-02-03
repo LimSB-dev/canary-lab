5aba07937566baf62456547303f2ba2445b70a62
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-02-04 01:37:45 +0900

    Refactor WeatherCard component and add loading and error handling

--- src/components/main/card/WeatherCard/index.tsx
+++ src/components/main/card/WeatherCard/index.tsx
@@ -1,9 +1,41 @@
+"use client";
+
+import useWeather from "@/hooks/useWeather";
 import styles from "./styles.module.scss";
+import useCity from "@/hooks/useCity";
 
 export const WeatherCard = () => {
+  const { city, loading: cityLoading, error: cityError } = useCity();
+  const {
+    weatherData,
+    loading: weatherLoading,
+    error: weatherError,
+  } = useWeather();
+
+  if (cityLoading || weatherLoading) {
+    return (
+      <article className={styles.card}>
+        <h5>Loading...</h5>
+      </article>
+    );
+  }
+
+  if (cityError || weatherError) {
+    return (
+      <article className={styles.card}>
+        <h5>Can not find you.</h5>
+      </article>
+    );
+  }
+
   return (
     <article className={styles.card}>
-      <h6>WeatherCard</h6>
+      <h4>{city}</h4>
+      <h2>{weatherData?.main.temp}°</h2>
+      <div className={styles.flex_row}>
+        <p>MAX: {weatherData?.main.tempMax.toFixed(1)}°</p>
+        <p>MIN: {weatherData?.main.tempMin.toFixed(1)}°</p>
+      </div>
     </article>
   );
 };

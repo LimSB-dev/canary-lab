20c928685bfe6eaa462aeadb299d067834310ab0
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-02-11 01:44:41 +0900

    Add login page to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,5 +17,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: `${APP_URL}/login`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
   ];
 }

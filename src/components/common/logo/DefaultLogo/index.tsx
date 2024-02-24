2aca51d479ea5a259cb186def07b65a964986772
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-02-24 22:35:26 +0900

    Update DefaultLogo size

--- src/components/common/logo/DefaultLogo/index.tsx
+++ src/components/common/logo/DefaultLogo/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export const DefaultLogo = () => {
   return (
     <Link href="/" passHref>
-      <Image src="/favicon.ico" alt="logo" width={16} height={16} />
+      <Image src="/favicon.ico" alt="logo" width={64} height={64} />
     </Link>
   );
 };

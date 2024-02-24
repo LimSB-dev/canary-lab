08f25b7dec81248d4be36095514b677324435251
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-02-24 22:35:22 +0900

    Update DefaultTerminal component styles

--- src/components/common/terminal/DefaultTerminal/index.tsx
+++ src/components/common/terminal/DefaultTerminal/index.tsx
@@ -6,14 +6,34 @@ import { useRouter } from "next/navigation";
 import styles from "./styles.module.scss";
 
 interface IProps {
+  /**
+   * The path to redirect when the command is entered or sequence is complete
+   */
   redirect: string;
+  /**
+   * The title of the terminal
+   */
   title: string;
+  /**
+   * The command to exit the terminal
+   */
   command: ICommand;
+  /**
+   * The sequence of text to display in the terminal or a function
+   */
   sequence: (string | number | (() => void))[];
 }
 
 interface ICommand {
+  /**
+   * The key to press to exit the terminal
+   * @example "Escape" or "x"
+   */
   key: string;
+  /**
+   * The name of the key to display in the terminal
+   * @example "ESC" or "X"
+   */
   name: string;
 }
 
@@ -30,23 +50,23 @@ const DefaultTerminal = ({ redirect, title, command, sequence }: IProps) => {
     return () => {
       window.removeEventListener("keydown", handleEsc);
     };
-  }, []);
+  }, [command.key, redirect, router]);
 
   return (
-    <div className={styles.terminal_loader}>
-      <div className={styles.terminal_header}>
-        <div className={styles.terminal_controls}>
-          <div className={`${styles.control} ${styles.close}`}></div>
-          <div className={`${styles.control} ${styles.minimize}`}></div>
-          <div className={`${styles.control} ${styles.maximize}`}></div>
-        </div>
-        <div className={styles.terminal_title}>{title}</div>
-        <div className={styles.terminal_command}>CMD + {command.name}</div>
-      </div>
-      <div className={styles.terminal_body}>
+    <article className={styles.terminal_loader}>
+      <header className={styles.terminal_header}>
+        <ul className={styles.terminal_controls}>
+          <li className={`${styles.control} ${styles.close}`}></li>
+          <li className={`${styles.control} ${styles.minimize}`}></li>
+          <li className={`${styles.control} ${styles.maximize}`}></li>
+        </ul>
+        <p className={styles.terminal_title}>{title}</p>
+        <nav className={styles.terminal_command}>CMD + {command.name}</nav>
+      </header>
+      <section className={styles.terminal_body}>
         <TypeAnimation sequence={sequence} />
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 

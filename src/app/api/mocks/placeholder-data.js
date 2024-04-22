439885c549abd4b79fec703eb9558ac6c90ae68e
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-04-23 01:32:41 +0900

    Update placeholder-data.js

--- src/app/api/mocks/placeholder-data.js
+++ src/app/api/mocks/placeholder-data.js
@@ -33,6 +33,106 @@ const posts = [
       },
     ],
   },
+  {
+    id: "58dc9e0b-712f-4377-85e9-fec4b6a6442b",
+    title: "Second Post",
+    status: "published",
+    createdAt: "2021-06-03T00:00:00.000Z",
+    updatedAt: "2021-06-03T00:00:00.000Z",
+    deletedAt: "",
+    likes: 10,
+    views: 20,
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "Second Post",
+          level: 1,
+        },
+      },
+      {
+        type: "paragraph",
+        data: {
+          text: "This is the second post.",
+        },
+      },
+    ],
+  },
+  {
+    id: "58dc9e0b-712f-4377-85e9-fec4b6a6442c",
+    title: "Third Post",
+    status: "published",
+    createdAt: "2021-06-03T00:00:00.000Z",
+    updatedAt: "2021-06-03T00:00:00.000Z",
+    deletedAt: "",
+    likes: 15,
+    views: 30,
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "Third Post",
+          level: 1,
+        },
+      },
+      {
+        type: "paragraph",
+        data: {
+          text: "This is the third post.",
+        },
+      },
+    ],
+  },
+  {
+    id: "58dc9e0b-712f-4377-85e9-fec4b6a6442d",
+    title: "Fourth Post",
+    status: "published",
+    createdAt: "2021-06-03T00:00:00.000Z",
+    updatedAt: "2021-06-03T00:00:00.000Z",
+    deletedAt: "",
+    likes: 20,
+    views: 40,
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "Fourth Post",
+          level: 1,
+        },
+      },
+      {
+        type: "paragraph",
+        data: {
+          text: "This is the fourth post.",
+        },
+      },
+    ],
+  },
+  {
+    id: "58dc9e0b-712f-4377-85e9-fec4b6a6442e",
+    title: "Fifth Post",
+    status: "published",
+    createdAt: "2021-06-03T00:00:00.000Z",
+    updatedAt: "2021-06-03T00:00:00.000Z",
+    deletedAt: "",
+    likes: 25,
+    views: 50,
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "Fifth Post",
+          level: 1,
+        },
+      },
+      {
+        type: "paragraph",
+        data: {
+          text: "This is the fifth post.",
+        },
+      },
+    ],
+  },
 ];
 
 module.exports = {

442571ff0eab960b283f4249d32824821fb80850
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-05-01 23:17:59 +0900

    게시물 테이블 스키마 변경 및 새로운 데이터 추가

--- src/app/api/mocks/placeholder-data.js
+++ src/app/api/mocks/placeholder-data.js
@@ -16,24 +16,10 @@ const posts = [
     status: "published",
     createdAt: "2021-06-03T00:00:00.000Z",
     updatedAt: "2021-06-03T00:00:00.000Z",
-    deletedAt: "",
+    deletedAt: null,
     likes: 5,
     views: 10,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "First Post",
-          level: 1,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "This is the first post.",
-        },
-      },
-    ],
+    content: "this is the first post",
   },
   {
     id: "58dc9e0b-712f-4377-85e9-fec4b6a6442b",
@@ -41,49 +27,10 @@ const posts = [
     status: "published",
     createdAt: "2021-06-03T00:00:00.000Z",
     updatedAt: "2021-06-03T00:00:00.000Z",
-    deletedAt: "",
+    deletedAt: null,
     likes: 10,
     views: 20,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "Second Post",
-          level: 1,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "This is the second post.",
-        },
-      },
-    ],
-  },
-  {
-    id: "58dc9e0b-712f-4377-85e9-fec4b6a6442c",
-    title: "Third Post",
-    status: "published",
-    createdAt: "2021-06-03T00:00:00.000Z",
-    updatedAt: "2021-06-03T00:00:00.000Z",
-    deletedAt: "",
-    likes: 15,
-    views: 30,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "Third Post",
-          level: 1,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "This is the third post.",
-        },
-      },
-    ],
+    content: "this is the second post",
   },
   {
     id: "58dc9e0b-712f-4377-85e9-fec4b6a6442d",
@@ -91,24 +38,10 @@ const posts = [
     status: "published",
     createdAt: "2021-06-03T00:00:00.000Z",
     updatedAt: "2021-06-03T00:00:00.000Z",
-    deletedAt: "",
+    deletedAt: null,
     likes: 20,
     views: 40,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "Fourth Post",
-          level: 1,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "This is the fourth post.",
-        },
-      },
-    ],
+    content: "this is the fourth post",
   },
   {
     id: "58dc9e0b-712f-4377-85e9-fec4b6a6442e",
@@ -116,24 +49,10 @@ const posts = [
     status: "published",
     createdAt: "2021-06-03T00:00:00.000Z",
     updatedAt: "2021-06-03T00:00:00.000Z",
-    deletedAt: "",
+    deletedAt: null,
     likes: 25,
     views: 50,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "Fifth Post",
-          level: 1,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "This is the fifth post.",
-        },
-      },
-    ],
+    content: "this is the fifth post",
   },
   {
     id: "58dc9e0b-712f-4377-85e9-fec4b6a6442f",
@@ -141,58 +60,10 @@ const posts = [
     status: "published",
     createdAt: "2024-04-27T00:00:00.000Z",
     updatedAt: "2024-04-27T00:00:00.000Z",
-    deletedAt: "",
+    deletedAt: null,
     likes: 1000,
     views: 2000,
-    blocks: [
-      {
-        type: "header",
-        data: {
-          text: "Level 1 Header",
-          level: 1,
-        },
-      },
-      {
-        type: "header",
-        data: {
-          text: "Level 2 Header",
-          level: 2,
-        },
-      },
-      {
-        type: "header",
-        data: {
-          text: "Level 3 Header",
-          level: 3,
-        },
-      },
-      {
-        type: "paragraph",
-        data: {
-          text: "This is the popular post.",
-        },
-      },
-      {
-        type: "list",
-        data: {
-          style: "ordered",
-          items: ["First item", "Second item", "Third item"],
-        },
-      },
-      {
-        type: "list",
-        data: {
-          style: "unordered",
-          items: ["First item", "Second item", "Third item"],
-        },
-      },
-      {
-        type: "code",
-        data: {
-          code: "console.log('Hello, world!')",
-        },
-      },
-    ],
+    content: "this is the popular post",
   },
 ];
 

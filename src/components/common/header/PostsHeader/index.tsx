e3de3c1d0b37979c840fe42b5ffe02bebf802100
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-05-04 01:45:20 +0900

    게시글 상세 페이지 레이아웃 수정 및 삭제 기능

--- src/components/common/header/PostsHeader/index.tsx
+++ src/components/common/header/PostsHeader/index.tsx
@@ -1,14 +1,40 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./styles.module.scss";
 import { DefaultLogo } from "@/components/common/logo";
+import { usePathname } from "next/navigation";
+import { useAppDispatch } from "@/hooks/reduxHook";
+import { deletePost } from "@/lib/fetch/posts";
 
 const PostsHeader = () => {
+  const isPosts = usePathname().endsWith("posts");
+  const id = usePathname().split("/")[2];
+
   return (
     <header className={styles.header}>
-      <section className={styles.content}>
+      <nav className={styles.nav}>
         <DefaultLogo size="small" />
-        <Link href="/create">Create</Link>
-      </section>
+        {isPosts && <Link href="/create">Create</Link>}
+        {!isPosts && <Link href="/posts">Posts</Link>}
+      </nav>
+      {!isPosts && (
+        <div className={styles.button_container}>
+          <Link
+            href={`/posts/${id}/edit`}
+            className={`button-card-shadow ${styles.edit_button}`}
+          >
+            edit
+          </Link>
+          <button
+            type="button"
+            className={`button-card-shadow ${styles.delete_button}`}
+            onClick={() => deletePost(id)}
+          >
+            delete
+          </button>
+        </div>
+      )}
     </header>
   );
 };

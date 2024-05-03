bdb2d8fd47c708fad2b144bb6241048ce52f736b
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-05-04 01:59:39 +0900

    입체적인 UI를 위한 카드 스타일 수정

--- src/components/main/card/ImageCardShadow/index.tsx
+++ src/components/main/card/ImageCardShadow/index.tsx
@@ -2,12 +2,18 @@ import styles from "./styles.module.scss";
 
 interface IProps {
   figure?: "square" | "circle";
+  canClick?: boolean;
 }
 
-export const ImageCardShadow = ({ figure = "square" }: IProps) => {
+export const ImageCardShadow = ({
+  figure = "square",
+  canClick = true,
+}: IProps) => {
   return (
     <div
-      className={styles.image_card_shadow}
+      className={`${styles.image_card_shadow} ${
+        canClick && "button-card-shadow"
+      }`}
       style={{
         borderRadius: figure === "circle" ? "50%" : "",
       }}

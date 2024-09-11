79b1bdcfcdb6967a2314569e1738666f0f5a24b0
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-09-12 02:07:59 +0900

    auth callbacks 추가

--- auth.ts
+++ auth.ts
@@ -7,16 +7,6 @@ import Kakao from "next-auth/providers/kakao";
 import { authConfig } from "./auth.config";
 import { sql } from "@vercel/postgres";
 
-async function getUser(email: string): Promise<IUser | undefined> {
-  try {
-    const user = await sql<IUser>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
-  } catch (error) {
-    console.error("Failed to fetch user:", error);
-    throw new Error("Failed to fetch user.");
-  }
-}
-
 export const {
   handlers: { GET, POST },
   auth,
@@ -25,4 +15,37 @@ export const {
 } = NextAuth({
   ...authConfig,
   providers: [GitHub, Apple, Google, Naver, Kakao],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        console.log(user);
+        return true;
+      } catch (error) {
+        console.error("Error handling signIn:", error);
+        return false; // 에러가 발생해도 세션을 반환합니다.
+      }
+    },
+    async session({ session }) {
+      try {
+        const userEmail = session.user.email;
+        const userName = session.user.name || "Unknown";
+        const userImage = session.user.image || null;
+
+        // 사용자 세션이 있을 때, 데이터베이스에서 사용자 정보를 업데이트합니다.
+        await sql`
+          INSERT INTO users (email, name, image, last_login, login_count)
+          VALUES (${userEmail}, ${userName}, ${userImage}, CURRENT_TIMESTAMP, 1)
+          ON CONFLICT (email) 
+          DO UPDATE SET 
+            last_login = CURRENT_TIMESTAMP, 
+            login_count = users.login_count + 1
+        `;
+
+        return session; // 세션 객체를 반환합니다.
+      } catch (error) {
+        console.error("Error updating session data:", error);
+        return session; // 에러가 발생해도 세션을 반환합니다.
+      }
+    },
+  },
 });

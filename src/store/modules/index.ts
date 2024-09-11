55848f0d0a6e66019c7650db3a819ccfe19cc067
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-09-12 02:10:46 +0900

    redux user state 추가

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -8,6 +8,7 @@ import weather from "./weather";
 import snackbar from "./snackbar";
 import location from "./location";
 import post from "./post";
+import user from "./user";
 
 const rootReducer = combineReducers({
   theme,
@@ -16,13 +17,22 @@ const rootReducer = combineReducers({
   snackbar,
   location,
   post,
+  user,
   // Add your reducers here
 });
 
 const persistConfig = {
   key: "root",
   storage,
-  whiteList: ["theme", "modal", "weather", "snackbar", "location", "post"],
+  whiteList: [
+    "theme",
+    "modal",
+    "weather",
+    "snackbar",
+    "location",
+    "post",
+    "user",
+  ],
 };
 
 const persistReducers = persistReducer(persistConfig, rootReducer);

8368e4ff37f95896b4dbaa347b43b3e44b3649d4
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-09-21 13:44:40 +0900

    feat: 로그아웃 확인 팝업 추가

---
+++ src/components/login/oauthLoginButton/OauthLoginButton.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+interface IOauthLoginButton {
+  provider: "github";
+  theme: "light" | "dark";
+  type: "default";
+}
+
+const OauthLoginButton = ({ provider, theme, type }: IOauthLoginButton) => {
+  return (
+    <Image
+      src={`/assets/oauth/${provider}_${theme}_${type}.svg`}
+      alt={`${provider}_${theme}_${type}`}
+      width={140}
+      height={30}
+    />
+  );
+};
+
+export default OauthLoginButton;

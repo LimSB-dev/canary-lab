881e94caa3f4e487e78988fc4f584d66d9b0f8f2
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2024-09-24 23:20:35 +0900

    게시물 카드 UI 수정 작업 (#69)
    * Refactor UI for post list items
    
    * 검색 결과 로딩 UI 추가

--- src/components/main/card/PostCard/PopularPostCard/index.tsx
+++ src/components/main/card/PostCard/PopularPostCard/index.tsx
@@ -1,16 +1,46 @@
 "use client";
 
-import styles from "./styles.module.scss";
 import Link from "next/link";
+import styles from "./styles.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faHeart as regularHeart,
+} from "@fortawesome/free-regular-svg-icons";
+import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { useAppSelector } from "@/hooks/reduxHook";
+import { isEmpty } from "lodash";
 
 interface IProps {
   popularPosts: IPost[];
 }
 
+const formatDate = (date: Date) =>
+  date.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "Asia/Seoul",
+  });
+
 const PopularPostCard = ({ popularPosts }: IProps) => {
-  const post = popularPosts
-    ? popularPosts[0]
-    : { index: null, title: "No Post" };
+  const user = useAppSelector((state) => state.user);
+
+  if (isEmpty(popularPosts)) {
+    return (
+      <Link
+        className={`button-card-shadow ${styles.card_popular}`}
+        href={"/create"}
+        passHref
+      >
+        <h6>게시물이 없습니다.</h6>
+      </Link>
+    );
+  }
+
+  const post = popularPosts[0];
+  const formattedDate = formatDate(post.createdAt);
+  const isLikedByUser = post.likes.includes(user.id);
 
   return (
     <Link
@@ -19,6 +49,15 @@ const PopularPostCard = ({ popularPosts }: IProps) => {
       passHref
     >
       <h6>{post.title}</h6>
+      <div className={styles.item_footer}>
+        <caption>{formattedDate}</caption>
+        <div className={styles.icon_container}>
+          <FontAwesomeIcon icon={isLikedByUser ? solidHeart : regularHeart} />
+          {post.likes.length}
+          <FontAwesomeIcon icon={faEye} />
+          {post.views}
+        </div>
+      </div>
     </Link>
   );
 };

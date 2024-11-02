36d8d7f3438c7bbec23f576ca559c7110daa807a
임성빈 <mae03087@naver.com>
2024-11-02 14:28:07 +0900

    최근 게시글 size, offset 설정

--- src/components/main/card/ResetCard/index.tsx
+++ src/components/main/card/ResetCard/index.tsx
@@ -2,13 +2,22 @@ import styles from "./styles.module.scss";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+import { useAppDispatch } from "@/hooks/reduxHook";
+import { setResetFlag } from "@/store/modules/post";
 
 export const ResetCard = () => {
+  const dispatch = useAppDispatch();
+
+  const handleReset = () => {
+    dispatch(setResetFlag());
+  };
+
   return (
     <button
       id="reset-card"
       type="button"
       className={`button-card-shadow ${styles.card}`}
+      onClick={() => handleReset()}
     >
       <FontAwesomeIcon icon={faArrowsRotate} size="lg" />
     </button>

d943c18305eb1a4790640948e99a9bf7b91feb43
임성빈 <mae03087@naver.com>
2024-11-03 19:49:19 +0900

    게시글 생성, 수정, 삭제, 취소 시 버튼 클릭 안되도록 수정

--- src/components/common/header/PostsHeader/index.tsx
+++ src/components/common/header/PostsHeader/index.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
-import styles from "./styles.module.scss";
-import { DefaultLogo } from "@/components/common/logo";
 import { usePathname } from "next/navigation";
+
+import styles from "./styles.module.scss";
+
+import { DefaultLogo } from "@/components/common/logo";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHook";
 import { setResetPost } from "@/store/modules/post";
 import { deletePost, postPost, putPost } from "@/app/api/posts";
@@ -12,6 +15,7 @@ const PostsHeader = () => {
   const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.user);
   const { title, markdownValue } = useAppSelector((state) => state.post);
+  const [loading, setLoading] = useState(false);
 
   const pathname = usePathname();
   const isAdmin = user.userType === "admin";
@@ -20,15 +24,29 @@ const PostsHeader = () => {
   const isEdit = pathname.endsWith("/edit");
   const index = Number(pathname.split("/")[2]);
 
-  const handleSave = async () => {
-    if (isEdit) await putPost({ index, title, markdownValue });
-    if (isCreate) await postPost({ title, markdownValue });
-    dispatch(setResetPost());
-  };
-
-  const handleDelete = async () => {
-    await deletePost(index);
-    dispatch(setResetPost());
+  const handleButton = async (
+    type: "create" | "edit" | "delete" | "cancel"
+  ) => {
+    try {
+      setLoading(true);
+      switch (type) {
+        case "create":
+          await postPost({ title, markdownValue });
+          break;
+        case "edit":
+          await putPost({ index, title, markdownValue });
+          break;
+        case "delete":
+          await deletePost(index);
+          break;
+        case "delete":
+        default:
+          break;
+      }
+      dispatch(setResetPost());
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -39,45 +57,52 @@ const PostsHeader = () => {
         {!isPosts && <Link href="/posts">POST</Link>}
       </nav>
 
-      {isAdmin && (
+      {isAdmin && !isPosts && (
         <div className={styles.button_container}>
-          {isPosts ? null : (
+          {isEdit || isCreate ? (
             <>
-              {!isEdit && !isCreate && (
-                <>
-                  <Link
-                    href={`/posts/${index}/edit`}
-                    className={`button-card-shadow ${styles.edit_button}`}
-                  >
-                    edit
-                  </Link>
-                  <button
-                    type="button"
-                    className={`button-card-shadow ${styles.delete_button}`}
-                    onClick={handleDelete}
-                  >
-                    delete
-                  </button>
-                </>
-              )}
-              {(isEdit || isCreate) && (
-                <>
-                  <button
-                    type="button"
-                    className={`card-shadow ${styles.submit_button}`}
-                    onClick={handleSave}
-                  >
-                    save
-                  </button>
-                  <Link
-                    href={isEdit ? `/posts/${index}` : "/posts"}
-                    className={`button-card-shadow ${styles.edit_button}`}
-                    onClick={() => dispatch(setResetPost())}
-                  >
-                    cancel
-                  </Link>
-                </>
-              )}
+              <button
+                type="button"
+                className={`card-shadow ${styles.submit_button} ${
+                  loading && styles.loading
+                }`}
+                onClick={() =>
+                  isEdit ? handleButton("edit") : handleButton("create")
+                }
+                disabled={loading}
+              >
+                {isEdit ? "save" : "create"}
+              </button>
+              <Link
+                href={isEdit ? `/posts/${index}` : "/posts"}
+                className={`button-card-shadow ${styles.edit_button} ${
+                  loading && styles.loading
+                }`}
+                onClick={() => handleButton("cancel")}
+              >
+                cancel
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                href={`/posts/${index}/edit`}
+                className={`button-card-shadow ${styles.edit_button} ${
+                  loading && styles.loading
+                }`}
+              >
+                edit
+              </Link>
+              <button
+                type="button"
+                className={`button-card-shadow ${styles.delete_button} ${
+                  loading && styles.loading
+                }`}
+                onClick={() => handleButton("delete")}
+                disabled={loading}
+              >
+                delete
+              </button>
             </>
           )}
         </div>

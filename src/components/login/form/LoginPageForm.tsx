ece0c694bfbf5eb451194969b087d25116de051e
임성빈 <mae03087@naver.com>
2025-03-30 10:42:29 +0900

    action 속성 타입 오류 해결

--- src/components/login/form/LoginPageForm.tsx
+++ src/components/login/form/LoginPageForm.tsx
@@ -1,25 +1,24 @@
 import { auth, signIn, signOut } from "../../../../auth";
 import OauthLoginButton from "@/components/login/oauthLoginButton/OauthLoginButton";
+import UserProfile from "@/components/login/form/userProfile";
 
-import styles from "./styles.module.scss";
-import UserProfile from "./userProfile";
+import styles from "@/styles.module.scss";
 
 const LoginPageForm = async () => {
   let session = await auth();
   let user = session?.user;
+
+  const handleSignIn = async (event: React.FormEvent) => {
+    event.preventDefault(); // 폼의 기본 동작(페이지 새로고침)을 방지합니다.
+    await signIn("github");
+  };
 
   return user ? (
     <div className={styles.user_profile_container}>
       <UserProfile user={user} />
     </div>
   ) : (
-    <form
-      className={styles.form}
-      action={async () => {
-        "use server";
-        await signIn("github");
-      }}
-    >
+    <form className={styles.form} onSubmit={handleSignIn}>
       <button className={styles.button} type="submit">
         <OauthLoginButton provider="github" theme="light" type="default" />
       </button>
